335ad604d3fbb033deda0103ef13739b3d353da3
Vitaliy <43200383+s04v@users.noreply.github.com>
2022-11-17 18:50:33 +0100

    Change database connection

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log(process.env);
+  console.log(process.env.DB_URL);
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
